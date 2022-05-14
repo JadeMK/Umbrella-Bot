255873bf18e3103dfbb0f45537555801bf8f7527
Jade Kim <JadeKim.dev@gmail.com>
2022-05-14 18:18:04 +0900

    Get weather data using OWM api

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Test Commit")
+import requests
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "Key here"
+
+# Geographical coordinates (lat: latitude, lon: longitude)
+params = {
+    "lat": 0,
+    "lon": 0,
+    "exclude": "current,minutely,daily",
+    "appid": api_key,
+}
+
+response = requests.get(url=OWM_endpoint, params=params)
+response.raise_for_status()
+weather_data = response.json()
+# Slice the data (next 12 hours)
+weather_hour = weather_data["hourly"][:12]
+
+# Check the weather condition
+for hour in weather_hour:
+    condition_code = int(hour["weather"][0]["id"])
+    # Codes below 700 means: rain, snow, drizzle or thunderstorm
+    if condition_code < 700:
+        # Send the umbrella reminder
