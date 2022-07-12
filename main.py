3a0a3c6e8d3bbbe4aa12bfebaa097e2d93bbba3e
Jade Kim <JadeKim.dev@gmail.com>
2022-07-12 22:50:37 +0900

    Bot: message

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 import os
 
+
 # Telegram Bot
 def bot_message(chat_id):
     token = os.environ.get("TELEGRAM_TOKEN")
-    message = "Hello World! (testing)"
+    message = "Don't forget your umbrella."
     send = "https://api.telegram.org/bot" + token + "/sendmessage?chat_id=" + chat_id + "&text=" + message
     bot_response = requests.get(send)
     return bot_response.json()
 
+
+chat_ids = "chat id here"
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = os.environ.get("OWM_API_KEY")
 
@@ -32,4 +35,4 @@ for hour in weather_hour:
     # Codes below 700 means: rain, snow, drizzle or thunderstorm
     if condition_code < 700:
         # Send the umbrella reminder
-        pass
+        bot_message(chat_ids)
